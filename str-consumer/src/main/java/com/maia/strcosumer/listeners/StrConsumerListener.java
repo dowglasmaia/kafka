b6ea9a19d44ab1e819465b538ba6d72f21a3fd86
Dowglas <dowglasmaia@gmail.com>
2023-06-11 10:58:04 -0300

    customizando o listener

--- str-consumer/src/main/java/com/maia/strcosumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/maia/strcosumer/listeners/StrConsumerListener.java
@@ -1,27 +1,37 @@
 package com.maia.strcosumer.listeners;
 
 
+import com.maia.strcosumer.listeners.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+
+    @StrConsumerCustomListener(groupId = "0")
     public void create(String message) {
         log.info("CREATE :: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "1")
     public void logs(String message) {
         log.info("LOG :: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "1")
     public void history(String message) {
         log.info("HISTORY :: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-0", topicPartitions = {
+            @TopicPartition(topic = "str-topic", partitions = {"1"})
+    }, containerFactory = "strContainerFactory")
+    public void analytics(String message) {
+        log.info("ANALYTICS :: Receive message {}", message);
     }
 
 }
