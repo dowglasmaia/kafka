7a1bcdbf02651ddb5c1a9ea1bb321bddc04e992b
Dowglas <dowglasmaia@gmail.com>
2023-06-14 09:50:07 -0300

    impl error handler

---
+++ str-consumer/src/main/java/com/maia/strcosumer/exceptions/ErrorCustomerHandler.java
@@ -0,0 +1,28 @@
+package com.maia.strcosumer.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class ErrorCustomerHandler implements KafkaListenerErrorHandler {
+
+
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
+        log.error("EXCEPTION_HANDLER ::: Exception Capturada!");
+
+        log.error("EXCEPTION_HANDLER ::: Exception Capturada!" + e.getMessage());
+
+        return null;
+    }
+
+//    @Override
+//    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
+//        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
+//    }
+}
--- str-consumer/src/main/java/com/maia/strcosumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/maia/strcosumer/listeners/StrConsumerListener.java
@@ -2,10 +2,13 @@ package com.maia.strcosumer.listeners;
 
 
 import com.maia.strcosumer.listeners.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
+
+import java.security.InvalidAlgorithmParameterException;
 
 @Log4j2
 @Component
@@ -13,9 +16,10 @@ public class StrConsumerListener {
 
 
     @KafkaListener(groupId = "group-0", topicPartitions = {
-            @TopicPartition(topic = "str-topic", partitions = {"1"}) }, containerFactory = "strContainerFactory")
+            @TopicPartition(topic = "str-topic", partitions = {"1"})}, containerFactory = "strContainerFactory")
     public void analytics(String message) {
         log.info("ANALYTICS :: Receive message {}", message);
+
     }
 
     @StrConsumerCustomListener(groupId = "0")
@@ -28,15 +32,24 @@ public class StrConsumerListener {
         log.info("LOG :: Receive message {}", message);
     }
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "1")
     public void history(String message) {
         log.info("HISTORY :: Receive message {}", message);
+
+        throw new InvalidAlgorithmParameterException("Error no HISTORY ");
     }
 
-
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validaMessageContainerFactory")
+    @SneakyThrows
+    @KafkaListener(
+            groupId = "group-2",
+            topics = "str-topic",
+            containerFactory = "validaMessageContainerFactory",
+            errorHandler = "errorCustomerHandler")
     public void inteceptor(String message) {
         log.info("INTECEPTOR :: Receive message {}", message);
+
+        throw new InvalidAlgorithmParameterException("Error no INTECEPTOR");
     }
 
 }
